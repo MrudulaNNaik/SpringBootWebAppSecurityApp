c338e3c2354400390ebe65bd634a4422e916f8dd
MRUDULA N NAIK <mrudulannaik1999@gmail.com>
2021-05-10 15:14:53 +0530

    initial commit

---
+++ SpringBootWebAppSecurity/src/main/java/com/example/demorest/SpringBootWebAppSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppSecurityApplication.class, args);
+	}
+
+}
